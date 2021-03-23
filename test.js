594528d4f95896fde4a7ac64afc3072c2bdcb54c
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 11:08:37 -0400

    remove chai

--- test.js
+++ test.js
@@ -1,28 +1,27 @@
-const { expect } = require('chai')
 const { compute } = require('./index.js')
 
 describe('FooBarQix', () => {
     it('should return 1 for 1', () => {
-        expect(compute(1)).to.equal(1)
+        expect(compute(1)).toEqual(1)
     });
 
     it('should return Foo for 3', () => {
-        expect(compute(3)).to.equal('Foo')
+        expect(compute(3)).toEqual('Foo')
     })
 
     it('should return Foo for 6', () => {
-        expect(compute(6)).to.equal('Foo')
+        expect(compute(6)).toEqual('Foo')
     })
 
     it('should return Bar for 5', () => {
-        expect(compute(5)).to.equal('Bar')
+        expect(compute(5)).toEqual('Bar')
     })
 
     it('should return Bar for 10', () => {
-        expect(compute(10)).to.equal('Bar')
+        expect(compute(10)).toEqual('Bar')
     })
 
     it('should return FooBar for 15', () => {
-        expect(compute(15)).to.equal('FooBar')
+        expect(compute(15)).toEqual('FooBar')
     })
 });
