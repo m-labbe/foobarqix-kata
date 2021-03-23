ec8821b6a6b3e87ad7dfc6e6f1b3d1f618b2ab7e
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 08:37:20 -0400

    write test for 3

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-const { expect } = require('chai');
-const { compute } = require('./index.js');
+const { expect } = require('chai')
+const { compute } = require('./index.js')
 
 describe('FooBarQix', () => {
     it('should return 1 for 1', () => {
-        expect(compute(1)).to.equal(1);
+        expect(compute(1)).to.equal(1)
     });
+
+    it('should return Foo for 3', () => {
+        expect(compute(3)).to.equal('Foo')
+    })
 });
