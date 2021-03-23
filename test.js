9bcf286d589917fe1105f4487a6c946d5b438059
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 08:38:00 -0400

    fix formatting

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const { expect } = require('chai');
-const { compute } = require('./index.js');
+const { expect } = require('chai')
+const { compute } = require('./index.js')
 
 describe('FooBarQix', () => {
     it('should return 1 for 1', () => {
-        expect(compute(1)).to.equal(1);
-    });
-});
+        expect(compute(1)).to.equal(1)
+    })
+})
