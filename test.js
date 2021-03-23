325c850a43ebc7fe34ce2dc1e150344831213eb6
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 11:05:27 -0400

    remove chai

--- test.js
+++ test.js
@@ -1,36 +1,35 @@
-const { expect } = require('chai')
-const { compute } = require('./index.js')
+const { compute, foobarqix } = require('./index.js')
 
 describe('FooBarQix', () => {
     it('should return 1 for 1', () => {
-        expect(compute(1)).to.equal(1)
+        expect(compute(1)).toEqual(1)
     });
 
     it('should return FooFoo for 3', () => {
-        expect(compute(3)).to.equal('FooFoo')
+        expect(compute(3)).toEqual('FooFoo')
     })
 
     it('should return Foo for 6', () => {
-        expect(compute(6)).to.equal('Foo')
+        expect(compute(6)).toEqual('Foo')
     })
 
     it('should return BarBar for 5', () => {
-        expect(compute(5)).to.equal('BarBar')
+        expect(compute(5)).toEqual('BarBar')
     })
 
     it('should return Bar for 10', () => {
-        expect(compute(10)).to.equal('Bar')
+        expect(compute(10)).toEqual('Bar')
     })
 
     it('should return FooBarBar for 15', () => {
-        expect(compute(15)).to.equal('FooBarBar')
+        expect(compute(15)).toEqual('FooBarBar')
     })
 
     it('should return QixQix for 7', () => {
-        expect(compute(7)).to.equal('QixQix')
+        expect(compute(7)).toEqual('QixQix')
     })
 
     it('should return Qix for 49', () => {
-        expect(compute(49)).to.equal('Qix')
+        expect(compute(49)).toEqual('Qix')
     })
 });
