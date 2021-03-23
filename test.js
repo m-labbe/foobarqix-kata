7e82f072477d215ff188d9cda82d9b758869b0cb
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 09:39:10 -0400

    update failing tests for contains

--- test.js
+++ test.js
@@ -6,28 +6,28 @@ describe('FooBarQix', () => {
         expect(compute(1)).to.equal(1)
     });
 
-    it('should return Foo for 3', () => {
-        expect(compute(3)).to.equal('Foo')
+    it('should return FooFoo for 3', () => {
+        expect(compute(3)).to.equal('FooFoo')
     })
 
     it('should return Foo for 6', () => {
         expect(compute(6)).to.equal('Foo')
     })
 
-    it('should return Bar for 5', () => {
-        expect(compute(5)).to.equal('Bar')
+    it('should return BarBar for 5', () => {
+        expect(compute(5)).to.equal('BarBar')
     })
 
     it('should return Bar for 10', () => {
         expect(compute(10)).to.equal('Bar')
     })
 
-    it('should return FooBar for 15', () => {
-        expect(compute(15)).to.equal('FooBar')
+    it('should return FooBarBar for 15', () => {
+        expect(compute(15)).to.equal('FooBarBar')
     })
 
-    it('should return Qix for 7', () => {
-        expect(compute(7)).to.equal('Qix')
+    it('should return QixQix for 7', () => {
+        expect(compute(7)).to.equal('QixQix')
     })
 
     it('should return Qix for 49', () => {
