347b4e4ec1e611d3f067fbd8933fe4a5f52f1892
Melissa Labbe <melissa-labbe@pluralsight.com>
2021-03-23 10:15:55 -0400

    implement contains logic

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const mapToString = new Map([
-    [3, 'Foo'],
-    [5, 'Bar'],
-    [7, 'Qix'],
+    ['3', 'Foo'],
+    ['5', 'Bar'],
+    ['7', 'Qix'],
 ])
 
 const compute = (input) => {
     let output = ''
+    const numbers = Array.from(mapToString.keys())
+    const vals = Array.from(mapToString.values())
     mapToString.forEach((val, key) => {
-        if(input % key === 0) {
+        if(input % parseInt(key) === 0) {
             output += val
+        }
+    })
+    input.toString().split('').forEach((char, i) => {
+        if(numbers.includes(char)) {
+            output += vals[numbers.indexOf(char)]
         }
     })
     return output === '' ? input : output
